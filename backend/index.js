59d6a7fc50f2c21356b00495b949959e6cbdd649
AngelaM288 <angelamartinezp@gmail.com>
2025-07-23 16:29:58 -0600

    Corrijo estructura y agrego render.yaml a la raíz

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 
-// ❌ COMENTADA TEMPORALMENTE LA CONEXIÓN A MONGO
+
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ Conectado a MongoDB Atlas'))
